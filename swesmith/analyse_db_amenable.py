f8d05a28481ee655c830f444949ba639cedc34fb
Atharv Sonwane <atharvs.twm@gmail.com>
2025-05-30 15:26:42 +0000

    better db amenable analysis script

--- swesmith/analyse_db_amenable.py
+++ swesmith/analyse_db_amenable.py
@@ -1,14 +1,24 @@
 import logging
+from pathlib import Path
+import html
 
 from athils import JsonLinesFile
 
-data = JsonLinesFile.read_from("data/swesmith/db_amenable_wo_fix_o3.jsonl")
+input_file = Path("data/swesmith/db_amenable_wo_fix_o3.jsonl")
+
+data = JsonLinesFile.read_from(input_file)
 
 yeses, nos, nones, errs = [], [], [], []
+
+content = ""
 
 for d in data:
     instance_id = d['instance_id']
     answer = d['selection_answer']
+
+    content += f"""<h3>{d['instance_id']}</h3>
+{html.escape(d['selection_response']).replace('\n', '<br>\n')}
+"""
     
     if answer == True:
         yeses.append(instance_id)
@@ -22,3 +32,17 @@ for d in data:
 
     
 print(f"{len(yeses)=}, {len(nos)=}, {len(nones)=}, {len(errs)=}")
+
+html_text = f"""<!DOCTYPE html>
+<html>
+<body>
+
+<h1 style="font-size:60px;">Outputs</h1>
+
+{content}
+
+</body>
+</html>"""
+
+output_file = input_file.parent / f"{input_file.stem}.html"
+output_file.write_text(html_text)
