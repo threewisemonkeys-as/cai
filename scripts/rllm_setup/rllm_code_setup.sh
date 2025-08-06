edffc4a4fd66cf1d361277f8200950550a1ffdd3
Atharv Sonwane <atharvs.twm@gmail.com>
2025-08-06 15:33:36 +0000

    fix git handling

--- scripts/rllm_setup/rllm_code_setup.sh
+++ scripts/rllm_setup/rllm_code_setup.sh
@@ -1,14 +1,15 @@
 export PATH="$HOME/.local/bin:$PATH"
 
-
 git clone https://github.com/threewisemonkeys-as/R2E-Gym.git
 cd R2E-Gym
-git checkout rllm2 --recurse-submodules
+git checkout rllm2
+git submodule update --init --recursive
 pip install --user -e .
 cd ..
-git clone --recurse-submodules https://github.com/threewisemonkeys-as/rllm.git
+git clone https://github.com/threewisemonkeys-as/rllm.git
 cd rllm
-git checkout rllm2 --recurse-submodules
+git checkout rllm2 
+git submodule update --init --recursive
 pip install --user -e ./verl[vllm]
 pip install --user -e .
 cd ..
--- scripts/rllm_setup/rllm_code_setup_rocm.sh
+++ scripts/rllm_setup/rllm_code_setup_rocm.sh
@@ -2,12 +2,14 @@ export PATH="$HOME/.local/bin:$PATH"
 
 git clone https://github.com/threewisemonkeys-as/R2E-Gym.git
 cd R2E-Gym
-git checkout rllm2_rocm --recurse-submodules
+git checkout rllm2_rocm 
+git submodule update --init --recursive
 pip install --user -e .
 cd ..
-git clone --recurse-submodules https://github.com/threewisemonkeys-as/rllm.git
+git clone https://github.com/threewisemonkeys-as/rllm.git
 cd rllm
-git checkout rllm2_rocm --recurse-submodules
+git checkout rllm2_rocm 
+git submodule update --init --recursive
 pip install --user -e ./verl[vllm]
 pip install --user -e .
 cd ..
