7264baad754da6c4993b1178696751522b15ff1e
Atharv Sonwane <atharvs.twm@gmail.com>
2025-08-06 13:37:29 +0000

    update scripts to use correct fork of submodules

--- scripts/rllm_setup/rllm_code_setup_rocm.sh
+++ scripts/rllm_setup/rllm_code_setup_rocm.sh
@@ -2,12 +2,12 @@ export PATH="$HOME/.local/bin:$PATH"
 
 git clone https://github.com/threewisemonkeys-as/R2E-Gym.git
 cd R2E-Gym
-git checkout rllm2_rocm
+git checkout rllm2_rocm --recurse-submodules
 pip install --user -e .
 cd ..
 git clone --recurse-submodules https://github.com/threewisemonkeys-as/rllm.git
 cd rllm
-git checkout rllm2_rocm
+git checkout rllm2_rocm --recurse-submodules
 pip install --user -e ./verl[vllm]
 pip install --user -e .
 cd ..
