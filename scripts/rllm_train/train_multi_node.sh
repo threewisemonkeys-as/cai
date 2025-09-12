5462d092539ee77b759e15e1a9f77280a84a3a8c
Isadora White <isadora.c.white@gmail.com>
2025-09-12 14:03:58 -0700

    fix the eval steps

--- scripts/rllm_train/train_multi_node.sh
+++ scripts/rllm_train/train_multi_node.sh
@@ -122,7 +122,7 @@ if [ "$NODE_RANK" -eq 0 ]; then
     trainer.n_gpus_per_node=${GPUS_PER_NODE} \
     trainer.nnodes=${NODES} \
     trainer.save_freq=5 \
-    trainer.test_freq=50 \
+    trainer.test_freq=10 \
     trainer.default_hdfs_dir=null \
     trainer.default_local_dir=${EXP_LOG_DIR} \
     env.name=swe \
