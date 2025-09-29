5b3dc1e4da41a0538d34eb12c6fbf7d4d3b294e7
Atharv Sonwane <atharvs.twm@gmail.com>
2025-09-29 08:37:14 -0700

    more data analysis scripts

--- data_analysis/bug_llm_type_analysis.py
+++ data_analysis/bug_llm_type_analysis.py
@@ -2,16 +2,24 @@ import json
 from pathlib import Path
 import random
 from collections import defaultdict
-
+import os
 
 from rich import print
 from rich.pretty import pprint
 from litellm import completion
+from tqdm import tqdm
 
 from r2egym.commit_models.diff_classes import ParsedCommit
 
+from dotenv import load_dotenv
+load_dotenv()
 
-MODEL = "openai/gpt-5-mini-2025-08-07"
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+AZURE_AD_TOKEN_PROVIDER = get_bearer_token_provider(DefaultAzureCredential(), os.getenv("AZURE_API_SCOPE", None))
+
+MODEL = "azure/gpt-5_2025-08-07"
+# MODEL = "openai/gpt-5-mini-2025-08-07"
 
 def get_batches(seq: list[str], n: int):
     for i in range(0, len(seq), n):
@@ -42,6 +50,7 @@ def llm_summarise_conv(
                 llm_repsonse = completion(
                     model=MODEL,
                     messages=[{"role": "user", "content": summary_prompt}],
+                    azure_ad_token_provider=AZURE_AD_TOKEN_PROVIDER,
                 )
                 summary = llm_repsonse["choices"][0]["message"]["content"]
                 if summary is not None:
@@ -81,6 +90,7 @@ Considering the given problem description and patch, summarise the bug in brief,
         llm_repsonse = completion(
             model=MODEL,
             messages=[{"role": "user", "content": prompt}],
+            azure_ad_token_provider=AZURE_AD_TOKEN_PROVIDER,
         )
         bs = llm_repsonse["choices"][0]["message"]["content"]
 
@@ -98,7 +108,7 @@ def categorise(guide: str, data: list[dict], sample_n: int | None = None):
         samples = random.sample(data, sample_n)
     else:
         samples = data
-    for bug in samples:   
+    for bug in tqdm(samples):
         if "patch" in bug:
             patch = bug['patch']
         elif "parsed_commit_content" in bug:
@@ -133,6 +143,7 @@ Alphabet code of category that bug falls into.
         llm_repsonse = completion(
             model=MODEL,
             messages=[{"role": "user", "content": prompt}],
+            azure_ad_token_provider=AZURE_AD_TOKEN_PROVIDER,
         )
         bs = llm_repsonse["choices"][0]["message"]["content"]
 
@@ -149,16 +160,17 @@ Alphabet code of category that bug falls into.
 
 def main():
     dataset_paths = [
-        "/home/msrt/data/rl_tasks/r2egym_train.json",
-        "/home/msrt/data/rl_tasks/swesmith_train.json",
-        "/home/msrt/data/rl_tasks/buggen_train.json",
-        "/home/msrt/data/rl_tasks/featadd_train.json",
+        "/home/v-asonwane/cai/data/rl_tasks/swebv.json"
+        # "/home/v-asonwane/cai/data/rl_tasks/buggen_train.json",
+        # "/home/v-asonwane/cai/data/rl_tasks/swesmith_train.json",
+        # "/home/v-asonwane/cai/data/rl_tasks/buggen_train.json",
+        # "/home/v-asonwane/cai/data/rl_tasks/featadd_train.json",
     ]
 
-    cumsum_output_path = Path("/home/msrt/atharv/data/llm_bugtype.json")
-    summary_output_path = Path("/home/msrt/atharv/data/bug_types.txt")
-    cresults_output_path = Path("/home/msrt/atharv/data/full_bug_type_results.json")
-    bug_type_counts_output_path = Path("/home/msrt/atharv/data/bug_type_counts.json")
+    cumsum_output_path = Path("/home/v-asonwane/cai/data/bug_type/llm_bugtype.json")
+    summary_output_path = Path("/home/v-asonwane/cai/data/bug_type/bug_types.txt")
+    cresults_output_path = Path("/home/v-asonwane/cai/data/bug_type/full_bug_type_results.json")
+    bug_type_counts_output_path = Path("/home/v-asonwane/cai/data/bug_type/bug_type_counts.json")
 
     pprint(dataset_paths)
 
@@ -194,6 +206,7 @@ def main():
 #     llm_response = completion(
 #         "openai/gpt-5",
 #         messages=[{"role": "user", "content": prompt}],
+#         azure_ad_token_provider=AZURE_AD_TOKEN_PROVIDER,
 #     )
 #     summary = llm_response["choices"][0]["message"]["content"]        
 
@@ -205,7 +218,7 @@ def main():
     bug_counts = {}
     for d_path in dataset_paths:
         data = json.load(open(d_path, "r"))
-        cresults = categorise(guide, data, sample_n=512)
+        cresults = categorise(guide, data)
         full_bug_type_results[d_path] = cresults
 
         ccounts = defaultdict(lambda: 0)
